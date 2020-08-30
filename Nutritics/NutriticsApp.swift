07991cd56d7ce27930218afe27a3f408ff82e04d
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2020-08-30 10:56:12 +0300

    Initial Commit

---
+++ Nutritics/NutriticsApp.swift
@@ -0,0 +1,17 @@
+//
+//  NutriticsApp.swift
+//  Nutritics
+//
+//  Created by Ramill Ibragimov on 30.08.2020.
+//
+
+import SwiftUI
+
+@main
+struct NutriticsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
